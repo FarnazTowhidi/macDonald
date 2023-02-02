9161d80835e52c5a9be556c95985a1d708813cfa
farnaz <farnaz.towhidi@gmail.com>
2023-02-01 21:12:36 -0500

    Deploy to Heroku

--- models/order.js
+++ models/order.js
@@ -1,18 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// const orderSchema = new Schema({
-//   date: {
-//     type: Date,
-//   },
-//   foods: [
-//     {
-//       foodID: Schema.Types.ObjectId,
-//       Quantity: Number,
-//     },
-//   ],
-// });
-
 const orderSchema = new Schema({
   date: {
     type: Date,
